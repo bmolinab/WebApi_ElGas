cde1569c9ced6af14321209a63ac9656fe31dbc5
Brian Molina <bmolina@digitalstrategy.com.ec>
2018-06-15 16:10:06 -0500

    Controler compra

---
+++ WebApi_ElGas/Controllers/ComprasController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi_ElGas.Context;
+
+namespace WebApi_ElGas.Controllers
+{
+    public class ComprasController : ApiController
+    {
+        private Model1 db = new Model1();
+
+        // GET: api/Compras
+        public IQueryable<Compra> GetCompra()
+        {
+            return db.Compra;
+        }
+
+        // GET: api/Compras/5
+        [ResponseType(typeof(Compra))]
+        public IHttpActionResult GetCompra(int id)
+        {
+            Compra compra = db.Compra.Find(id);
+            if (compra == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(compra);
+        }
+
+        // PUT: api/Compras/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCompra(int id, Compra compra)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != compra.IdCompra)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(compra).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CompraExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Compras
+        [ResponseType(typeof(Compra))]
+        public IHttpActionResult PostCompra(Compra compra)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Compra.Add(compra);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = compra.IdCompra }, compra);
+        }
+
+        // DELETE: api/Compras/5
+        [ResponseType(typeof(Compra))]
+        public IHttpActionResult DeleteCompra(int id)
+        {
+            Compra compra = db.Compra.Find(id);
+            if (compra == null)
+            {
+                return NotFound();
+            }
+
+            db.Compra.Remove(compra);
+            db.SaveChanges();
+
+            return Ok(compra);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CompraExists(int id)
+        {
+            return db.Compra.Count(e => e.IdCompra == id) > 0;
+        }
+    }
+}
